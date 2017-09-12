c5551b617f7b52f5504d299908c2dc159a904767
Luiz Carvalho <lucarval@redhat.com>
2017-09-12 14:21:39 -0400

    Create a fake arch for testing multi-arch builds

--- client/bin/setup.sh
+++ client/bin/setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -xeuo pipefail
 
-# Koji CLI in RHEL7 has a nasty bug - it attempts to verify that user is 
+# Koji CLI in RHEL7 has a nasty bug - it attempts to verify that user is
 # package owner before auth procedure
 # Lets quickly patch it
 
@@ -9,8 +9,8 @@ sed -i "s;if not options\.owner;activate_session(session)\n    if not options\.o
 sed -i -e '701d;' /usr/bin/koji
 
 # TODO: Just create directly in DB?
-koji add-tag build --arches=x86_64
-koji add-tag dest --arches=x86_64
+koji add-tag build --arches=x86_64,amd64
+koji add-tag dest --arches=x86_64,amd64
 koji add-target candidate build dest
 
 #sleep infinity
@@ -59,6 +59,9 @@ oc adm policy add-role-to-user osbs-custom-build osbs -z builder --role-namespac
 
 oc secrets new-dockercfg v2-registry-dockercfg --docker-server=172.17.0.1:5000 --docker-username=osbs --docker-password=craycray --docker-email=test@test.com
 
+cp /configs/reactor-config-secret.yml /tmp/config.yaml
+cp /configs/client-config-secret.conf /tmp/osbs.conf
+
 oc new-project worker
 oc adm policy add-role-to-user edit -z builder
 
@@ -87,11 +90,40 @@ oc adm policy add-role-to-user osbs-custom-build osbs -z builder --role-namespac
 
 oc secrets new-dockercfg v2-registry-dockercfg --docker-server=172.17.0.1:5000 --docker-username=osbs --docker-password=craycray --docker-email=test@test.com
 
-token=$(oc whoami -t)
+token_x86_64=$(oc whoami -t)
 
-cp /configs/reactor-config-secret.yml /tmp/config.yaml
-cp /configs/client-config-secret.conf /tmp/osbs.conf
-sed -i "s/OSBS_TOKEN/${token}/" /tmp/osbs.conf
+oc new-project worker-amd64
+oc adm policy add-role-to-user edit -z builder
+
+oc secret new kojisecret \
+    serverca=/opt/koji-clients/kojiadmin/serverca.crt \
+    ca=/opt/koji-clients/kojiadmin/clientca.crt \
+    cert=/opt/koji-clients/kojiadmin/client.crt
+
+oc secrets add serviceaccount/builder secrets/kojisecret --for=mount
+
+oc create policybinding worker-amd64
+
+oc create -f - << EOF
+apiVersion: v1
+kind: Role
+metadata:
+  name: osbs-custom-build
+rules:
+- verbs:
+  - create
+  resources:
+  - builds/custom
+EOF
+
+oc adm policy add-role-to-user osbs-custom-build osbs -z builder --role-namespace worker-amd64
+
+oc secrets new-dockercfg v2-registry-dockercfg --docker-server=172.17.0.1:5000 --docker-username=osbs --docker-password=craycray --docker-email=test@test.com
+
+token_amd64=$(oc whoami -t)
+
+sed -i "s/OSBS_x86_64_TOKEN/${token_x86_64}/" /tmp/osbs.conf
+sed -i "s/OSBS_amd64_TOKEN/${token_amd64}/" /tmp/osbs.conf
 sed -i "s/KOJI_HUB_IP/${WORKSTATION_IP}/" /tmp/osbs.conf
 
 oc project osbs
